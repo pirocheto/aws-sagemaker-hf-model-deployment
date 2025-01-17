2f78e2a09ae9a5672d924b178ee67ed8888e2c38
pirocheto <pirocheto@gmail.com>
2025-01-17 18:38:55 +0100

    add variant name

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,15 +3,16 @@ locals {
 }
 
 module "model" {
-  source               = "./modules/aws-sagemaker-hf-pytorch-inference-model-deployment"
-  model_name           = "${local.env}-multilingual-sentiment-analysis"
-  python_version       = "py310"
-  ubuntu_version       = "22.04"
-  transformers_version = "4.37.0"
-  pytorch_version      = "2.1.0"
-  model_bucket_name    = "sagemaker-us-east-1-639269844451"
-  model_bucket_key     = "${local.env}/tabularisai/multilingual-sentiment-analysis/model.tar.gz"
-  instance_type        = "ml.m5.large"
-  memory_size_in_mb    = 2048
-  max_concurrency      = 1
+  source                = "./modules/aws-sagemaker-hf-pytorch-inference-model-deployment"
+  model_name            = "${local.env}-multilingual-sentiment-analysis"
+  endpoint_variant_name = "variant-1"
+  python_version        = "py310"
+  ubuntu_version        = "22.04"
+  transformers_version  = "4.37.0"
+  pytorch_version       = "2.1.0"
+  model_bucket_name     = "sagemaker-us-east-1-639269844451"
+  model_bucket_key      = "${local.env}/tabularisai/multilingual-sentiment-analysis/model.tar.gz"
+  instance_type         = "ml.m5.large"
+  memory_size_in_mb     = 2048
+  max_concurrency       = 1
 }
